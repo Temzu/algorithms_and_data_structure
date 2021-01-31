b504726de5aa8e100a46c57d07c52455bf923e38
Artem <40913z@mail.ru>
2021-01-31 21:39:52 +0300

    second task

--- src/main/java/com/geekbrains/temzu/homeworks/lesson_5/Main.java
+++ src/main/java/com/geekbrains/temzu/homeworks/lesson_5/Main.java
@@ -1,17 +1,60 @@
 package com.geekbrains.temzu.homeworks.lesson_5;
 
+
 public class Main {
     //Рекурсия
 //    1. Написать программу по возведению числа в степень с помощью рекурсии.
 //    2. * Задача о шахматном короле
-//    3. * Задача о восьми ферзях
 
+    private static int move = 1;
+    private static int[][] desk = new int[8][8];
 
     public static void main(String[] args) {
         System.out.println(pow(2, 5));
+        System.out.println();
+
+
+
+
+        //3432 вариантов
+        king(desk, 8);
+        System.out.println("move = " + move);
+
+        for (int i = 0; i < desk.length; i++) {
+            for (int j = 0; j < desk[i].length; j++) {
+                System.out.printf("%5d", desk[i][j]);
+            }
+            System.out.println();
+        }
+
+
     }
 
-    public static int pow(int a, int pow) {
+    private static void step() {
+        move++;
+    }
+
+    private static void king(int[][] desk, int bound) {
+        int tmp = 1;
+        if (bound == 0) {
+            step();
+        } else {
+            king(desk, --bound);
+            for (int y = bound; y < desk.length; y++) {
+                for (int x = bound; x < desk[y].length; x++) {
+                    if (x == 0 || y == 0) {
+                        desk[y][x] = 1;
+                    } else {
+                        tmp = move;
+                        desk[y][x] = desk[y][x - 1] + desk[y - 1][x];
+                    }
+                }
+            }
+            move += tmp;
+        }
+    }
+
+    private static int pow(int a, int pow) {
         if (pow == 1) {
             return a;
         } else {
