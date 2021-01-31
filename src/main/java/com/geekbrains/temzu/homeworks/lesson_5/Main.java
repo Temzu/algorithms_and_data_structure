aace60d275246539534304ce242c64de23beee81
Artem <40913z@mail.ru>
2021-01-31 18:09:16 +0300

    first task

--- src/main/java/com/geekbrains/temzu/homeworks/lesson_5/Main.java
+++ src/main/java/com/geekbrains/temzu/homeworks/lesson_5/Main.java
@@ -1,7 +1,21 @@
 package com.geekbrains.temzu.homeworks.lesson_5;
 
 public class Main {
+    //Рекурсия
+//    1. Написать программу по возведению числа в степень с помощью рекурсии.
+//    2. * Задача о шахматном короле
+//    3. * Задача о восьми ферзях
+
+
     public static void main(String[] args) {
-        
+        System.out.println(pow(2, 5));
+    }
+
+    public static int pow(int a, int pow) {
+        if (pow == 1) {
+            return a;
+        } else {
+            return a * pow(a, --pow);
+        }
     }
 }
